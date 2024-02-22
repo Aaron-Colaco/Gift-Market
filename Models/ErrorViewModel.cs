b3eef91ecf2dc474c6cc25a8e0917d40f606534f
ac121654 <ac121654@8CG8163KZN>
2024-02-22 13:43:12 +1300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AaronColacoAsp.NETProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
