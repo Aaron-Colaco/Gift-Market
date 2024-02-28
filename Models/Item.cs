47f5fcaf90e7990ed3c13e7cc270a0ad857af856
Aaron-Colaco <ac121654@avcol.school.nz>
2024-02-28 14:54:38 +1300

    Today I started developing my project. I started off by creating a Class for the item entity. I made sure to plan out all the data properties/fields in my class, this will help save time and reduce possible errors. I created my project in Visual Studio 2022 using an Asp.net MVC(Model view Controller) solution for my web application utilising the .net 8 framework.

---
+++ Models/Item.cs
@@ -0,0 +1,17 @@
+﻿namespace AaronColacoAsp.NETProject.Models
+{
+
+        public class Items
+        {
+
+            public int ItemID { get; set; }
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+            public decimal CostToProduce { get; set; }
+            public string ImageURL { get; set; }
+
+        }
+
+ 
+}
+
