d90f75799e070a05109da7376fd72137003ea434
Aaron-Colaco <ac121654@avcol.school.nz>
2024-04-13 19:04:53 +1200

    I improved the vilidaiton on the checkout form by adding a regx expression for phone number fields. I also worked on the CSS to make it more presentable and optimized for smaller devices.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using AaronColacoAsp.NETProject.Data;
+using AaronColacoAsp.NETProject.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,7 +17,7 @@ namespace AaronColacoAsp.NETProject
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            builder.Services.AddDefaultIdentity<Customer>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
 
