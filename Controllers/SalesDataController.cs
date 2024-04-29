bb7a92c17afe9bbed56081c1a199218e7acba797
Aaron-Colaco <ac121654@avcol.school.nz>
2024-04-30 10:05:47 +1200

    Fixed validation on check out form

--- Controllers/SalesDataController.cs
+++ Controllers/SalesDataController.cs
@@ -23,9 +23,9 @@ namespace AaronColacoAsp.NETProject.Controllers
         public IActionResult SalesDashboard(DateTime Date1, DateTime Date2)
         {
 
-            var OrderData = _context.Order.Where(a => a.OrderTime >= Date1 && a.OrderTime <= Date2);
-            var GiftData = _context.Gift.Where(a => a.Order.OrderTime >= Date1 && a.Order.OrderTime <= Date2); ;
-            var OrderItemData = _context.OrderItem.Where(a => a.Orders.OrderTime >= Date1 && a.Orders.OrderTime <= Date2); ;
+            var OrderData = _context.Order.Where(a => a.OrderTime >= Date1 && a.OrderTime <= Date2).Where(a => a.StatusId !=1);
+            var GiftData = _context.Gift.Where(a => a.Order.OrderTime >= Date1 && a.Order.OrderTime <= Date2).Where(a => a.Order.StatusId != 1);
+            var OrderItemData = _context.OrderItem.Where(a => a.Orders.OrderTime >= Date1 && a.Orders.OrderTime <= Date2).Where(a => a.Orders.StatusId != 1); ; 
 
            
             decimal TotalSales = OrderItemData.Sum(a => a.Items.Price * a.Quantity);
